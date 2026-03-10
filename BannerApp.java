25c723bd4e4b6167e1ff8dad870ebf3418246a79
DharnidharPandey23 <dharnidhar3@gmail.com>
2026-03-10 22:51:55 +0530

    Update BannerApp.java

--- BannerApp.java
+++ BannerApp.java
@@ -1,50 +1,14 @@
-import java.util.*;
-
-class CharacterPattern {
-
-    private Map<Character, String[]> patterns;
-
-    public CharacterPattern() {
-        patterns = new HashMap<>();
-
-        patterns.put('O', new String[]{
-                " *** ",
-                "*   *",
-                "*   *",
-                "*   *",
-                " *** "
-        });
-
-        patterns.put('P', new String[]{
-                "**** ",
-                "*   *",
-                "**** ",
-                "*    ",
-                "*    "
-        });
-
-        patterns.put('S', new String[]{
-                " ****",
-                "*    ",
-                " *** ",
-                "    *",
-                "**** "
-        });
-    }
-
-    public void printBanner(String text) {
-        for (int row = 0; row < 5; row++) {
-            for (char ch : text.toCharArray()) {
-                System.out.print(patterns.get(ch)[row] + "  ");
-            }
-            System.out.println();
-        }
-    }
-}
-
 public class BannerApp {
+
     public static void main(String[] args) {
-        CharacterPattern banner = new CharacterPattern();
-        banner.printBanner("OOPS");
+
+        System.out.println(" *****   *****   *****    ***** ");
+        System.out.println("*     * *     * *     *  *     *");
+        System.out.println("*     * *     * *     *  *      ");
+        System.out.println("*     * *     * *****    *****  ");
+        System.out.println("*     * *     * *             * ");
+        System.out.println("*     * *     * *      *     *  ");
+        System.out.println(" *****   *****  *       *****   ");
+
     }
 }
