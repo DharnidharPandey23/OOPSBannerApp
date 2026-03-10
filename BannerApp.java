a9a3e417a8b9fc5f746dcbb004f922f13d815b43
DharnidharPandey23 <dharnidhar3@gmail.com>
2026-03-11 00:16:02 +0530

    Update BannerApp.java

--- BannerApp.java
+++ BannerApp.java
@@ -2,18 +2,51 @@ public class BannerApp {
 
     public static void main(String[] args) {
 
-        String[] banner = {
-                String.join("   ", " ***** ", " ***** ", " ***** ", " ***** "),
-                String.join("   ", "*     *", "*     *", "*     *", "*     *"),
-                String.join("   ", "*     *", "*     *", "*     *", "*"),
-                String.join("   ", "*     *", "*     *", " ***** ", " ***** "),
-                String.join("   ", "*     *", "*     *", "*", "      *"),
-                String.join("   ", "*     *", "*     *", "*   *", "*     *"),
-                String.join("   ", " ***** ", " ***** ", "*", " ***** ")
-        };
+        String[] o = getO();
+        String[] p = getP();
+        String[] s = getS();
 
-        for (String line : banner) {
-            System.out.println(line);
+        for (int i = 0; i < 7; i++) {
+            System.out.println(o[i] + "   " + o[i] + "   " + p[i] + "   " + s[i]);
         }
+    }
+
+    // Method for letter O
+    public static String[] getO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    // Method for letter P
+    public static String[] getP() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    // Method for letter S
+    public static String[] getS() {
+        return new String[]{
+                " ***** ",
+                "*     ",
+                "*     ",
+                " ***** ",
+                "     *",
+                "     *",
+                "***** "
+        };
     }
 }
